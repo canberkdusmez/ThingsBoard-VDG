e313a1c0de29857db5909efd9d630bc98aa67746
canberkdusmez <canberkdusmez@gmail.com>
2024-07-10 14:55:47 +0300

    Add files via upload

--- TB_VDG/TB_VDG.py
+++ TB_VDG/TB_VDG.py
@@ -5,23 +5,16 @@ import requests
 
 url = "http://YOURDOMAIN/api/v1/YOURTOKEN/telemetry"
 
-data = []
-
-for j in range (0,100) :
-    random_number = random.randint(190,300)
-
-    datas = {"v":random_number}
-    data.append(datas)
+data = [{"v": random.randint(190, 300)} for _ in range(100)]
 
 with open('data.json','w') as j_files:
     json.dump(data,j_files,indent=4)
 with open ("data.json","r") as j_files :
     file = json.load(j_files)
 
-loop = 0
-for loop in range (0,100) :
+for item in file :
 
-    send = requests.post(url,json=file)
+    send = requests.post(url,json=item)
 
     if send.status_code == 200 :
         print ("Request succesfull.")
