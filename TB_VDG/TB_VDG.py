fbf5be1e1d3c6fe1853b53483d84b54f46636f1b
canberkdusmez <canberkdusmez@gmail.com>
2024-07-10 16:43:13 +0300

    Add files via upload

--- TB_VDG/TB_VDG.py
+++ TB_VDG/TB_VDG.py
@@ -1,20 +1,43 @@
 import time
+import datetime as dt
 import random
 import json
 import requests
 
 url = "http://YOURDOMAIN/api/v1/YOURTOKEN/telemetry"
 
-data = [{"v": random.randint(190, 300)} for _ in range(100)]
+data = []
 
-with open('data.json','w') as j_files:
-    json.dump(data,j_files,indent=4)
-with open ("data.json","r") as j_files :
-    file = json.load(j_files)
+a=190
+b=300
+c=0
 
-for item in file :
+for j in range (0,100) :
+    random_number = random.randint(a,b)
+    time_rn = dt.datetime.now()
+    time_rn_j = time_rn.isoformat()
+    datas = {
+        "v":random_number,
+        "RequestTime":time_rn_j,
+        }
+    data.append(datas)
+    c = random_number/10
+    a = random_number-c
+    b = random_number+c
+    a = round(a)
+    b = round(b)
+    if a < 190 :
+        a = 190
+    if b > 300 :
+        b = 300
 
-    send = requests.post(url,json=item)
+    with open('data.json','w') as j_files:
+        json.dump(data,j_files,indent=4)
+
+    print ("Voltage:",random_number)
+    print ("Time:",time_rn)
+
+    send = requests.post(url,json=datas)
 
     if send.status_code == 200 :
         print ("Request succesfull.")
